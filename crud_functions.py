6b1aae44a8beda32042a41d79b5c61704f0f7dcf
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-10-03 15:28:56 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,22 +1,22 @@
 import sqlite3
 
-
 def initiate_db():
-    connection = sqlite3.connect('database.db')
+    connection = sqlite3.connect('products.db')
     cursor = connection.cursor()
 
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products(
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT,
-    price INT NOT NULL
+    price INTEGER NOT NULL
     )
     ''')
-
+    connection.commit()
+    connection.close()
 
 def get_all_products():
-    connection = sqlite3.connect('database.db')
+    connection = sqlite3.connect('products.db')
     cursor = connection.cursor()
 
     cursor.execute('''
@@ -26,3 +26,5 @@ def get_all_products():
     connection.commit()
     connection.close()
     return products
+
+
