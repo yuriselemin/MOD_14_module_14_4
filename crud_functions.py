b51cf66aeea7a66628309d2eeeecc4c7b107739d
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-10-03 17:13:00 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,33 +1,3 @@
-# import sqlite3
-#
-#
-# def initiate_db():
-#     connection = sqlite3.connect('products.db')
-#     cursor = connection.cursor()
-#
-#     cursor.execute('''
-#     CREATE TABLE IF NOT EXISTS Products(
-#     id INT PRIMARY KEY,
-#     title TEXT NOT NULL,
-#     description TEXT,
-#     price INT NOT NULL
-#     )
-#     ''')
-#
-#
-# def get_all_products():
-#     connection = sqlite3.connect('products.db')
-#     cursor = connection.cursor()
-#
-#     cursor.execute('''
-#     SELECT * FROM Products
-#     ''')
-#     products = cursor.fetchall()
-#     connection.commit()
-#     connection.close()
-#     return products
-#
-
 
 import sqlite3
 
